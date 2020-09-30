8bbad04dee8acd827334cbaa90881b7df84f5550
JuanPaS <juan.sanchez@newshore.es>
2020-09-29 22:01:13 -0500

    implementation examples

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
+// copy
+
 const copyService = require('./libraries/copy');
 
-copyService.copyFiles('./libraries/', './lib/', 'ts');
+const copyServiceExample = () => {
+  copyService.copyFiles('./libraries/', './lib/', 'ts');
+};
+
+// processJsonFiles
+
+const processJsonFilesService = require('./libraries/processJsonFiles');
+
+const processJsonFilesServiceExample = () => {
+  processJsonFilesService.overrideFiles(['./example/file.json'], (json) => {
+    for (let i = 0; i < json.data.length; i++) {
+      const element = json.data[i];
+
+      json['data' + element] = element;
+    }
+
+    return json;
+  });
+};
+
+// processJsonFolder
+
+const processJsonFolderService = require('./libraries/processJsonFolder');
+
+const processJsonFolderServiceExample = () => {
+  processJsonFolderService.overrideFolder('./example/', (json) => {
+    for (let i = 0; i < json.data.length; i++) {
+      const element = json.data[i];
+
+      json['data' + element] = element;
+    }
+
+    return json;
+  });
+};
+
+// processStreamFiles
+
+const processStreamFilesService = require('./libraries/processStreamFiles');
+
+const processStreamFilesServiceExample = () => {
+  processStreamFilesService.processStreamToArrayJson(['./example/stream.txt', './example/stream copy.txt']);
+};
+
+// copyServiceExample();
+// processJsonFilesServiceExample();
+// processJsonFolderServiceExample();
+processStreamFilesServiceExample();
